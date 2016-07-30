dd54f2554d64197596ad05ef69573ce31655caf0
Manan Mehta <mananmehta@JARVIS.local>
2016-07-31 01:38:19 +0530

    create Exceptions.py for custom exception definitions

---
+++ Exceptions.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+"""
+Exceptions.py
+
+This module contains all the Exception class definitions used in 
+other LinearRegression scripts
+"""
+
+class NoDataException(Exception):
+    pass
+
+class DataHandlingException(Exception):
+    pass
